2c4c2ca8448290247932e0d17c51bb727e347b53
MaRS-208 <manuela.riossosa@gmail.com>
2024-04-14 15:36:19 -0300

    0. Initialize new project XI

--- csharp-hello_world/0-new_project/Program.cs
+++ csharp-hello_world/0-new_project/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace 0-new_project
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
