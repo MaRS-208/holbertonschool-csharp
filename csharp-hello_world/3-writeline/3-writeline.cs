b8acf6ebf4e2350e3e2d5c0a090324e4a1939ed6
MaRS-208 <manuela.riossosa@gmail.com>
2024-04-14 16:39:29 -0300

    3. Hello, WriteLine I

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("\"Programming is like building a multilingual puzzle");
+﻿using System;
+
+namespace _3_new_project
+{
+        class Program
+        {
+                static void Main(string[] args)
+                {
+                        Console.WriteLine("\"Programming is like building a multilingual puzzle");
+                }
+        }
+}
