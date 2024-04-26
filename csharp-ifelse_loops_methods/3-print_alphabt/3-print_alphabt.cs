a04cdfdb0a5006c03128469622412820531b094e
MaRS-208 <manuela.riossosa@gmail.com>
2024-04-26 14:44:45 -0300

    3. When I was having that alphabet soup, I never thought that it would pay off

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+	class Program
+	{
+	    	static void Main(string[] args)
+		{
+			for (char alpha = 'a'; alpha <= 'z'; alpha ++)
+			{
+				if(alpha != 'e' && alpha != 'q')
+				{
+					Console.Write(alpha);
+				}
+			}
+		}
+	}
+}
